9aefd1da98cbada893e6e750914ad8d383e05ddd
Katerina Boididou <mpoikaterina@gmail.com>
2020-04-14 13:35:30 +0100

    Added mz_mean field in Roi class

--- roi_handler.py
+++ roi_handler.py
@@ -1,6 +1,10 @@
+import numpy as np
+
+
 class ROI:
     def __init__(self, mz_values):
         self.mz_values = mz_values
         self.intensity_values = []
         self.rt_values = []
         self.extended = False
+        self.mz_mean = np.mean(mz_values)
