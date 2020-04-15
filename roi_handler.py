56a9f13ebab86b83e669d5dbb9d771ad7672dab1
Katerina Boididou <mpoikaterina@gmail.com>
2020-04-15 16:08:57 +0100

    Corrected funstions and added get_extended function

--- roi_handler.py
+++ roi_handler.py
@@ -12,12 +12,21 @@ class ROI:
     def get_mz_values(self):
         return self.mz_values
 
-    def set_mz_values(self, mz_values):
-        self.mz_values = mz_values
+    def add_mz_value(self, mz_value):
+        self.mz_values.append(mz_value)
 
     def get_mz_mean(self):
         return self.mz_mean
 
     def update_mz_mean(self):
         self.mz_mean = np.mean(self.mz_values)
+
+    def get_extended(self):
+        return self.extended
+
+    def set_extended(self, extended):
+        self.extended = extended
+
+
+
 
